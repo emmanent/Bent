c11b7bd0c0b3d8fd462f4cb29a5cc9c0e809c9c4
Emma <emmanentli@gmail.com>
2023-01-14 14:46:52 -0800

    Populate CreateYourAccount screen

--- screens/createYourAccount.tsx
+++ screens/createYourAccount.tsx
@@ -1,18 +1,67 @@
-import React from "react";
-import { Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useRef } from "react";
+import { View, Text, TextInput, Alert, Button } from "react-native";
+import PhoneInput from "react-native-phone-number-input";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { StatusBar } from "expo-status-bar";
 
 type CreateYourAccountProps = {
   navigation: any;
-}
+};
 
 const CreateYourAccount: React.FC<CreateYourAccountProps> = (props) => {
-    return (
-        <View>
-          <Text>Create Your Account</Text>
-          <StatusBar/>
-        </View>
-      );
-}
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const today = new Date(Date.now())
+  const [dob, setDob] = useState<Date>(today);
+  const phoneInput = useRef(null);
+
+  return (
+    <View>
+      <Text>Create Your Account</Text>
+
+      <Text>Phone Number</Text>
+      <PhoneInput
+        ref={phoneInput}
+        defaultValue={phoneNumber}
+        defaultCode="IN"
+        layout="first"
+        withShadow
+        autoFocus
+        textContainerStyle={{ paddingVertical: 0 }}
+        onChangeFormattedText={(text) => {
+          setPhoneNumber(text);
+        }}
+      />
+
+      <Text>Email</Text>
+      <TextInput onChangeText={(text) => setEmail(text)} value={email} />
+
+      <Text>Date of Birth</Text>
+      <DateTimePicker
+        testID="dateTimePicker"
+        timeZoneOffsetInMinutes={0}
+        value={dob}
+        mode="date"
+        is24Hour={true}
+        display="default"
+        onChange={(_, date) => setDob(date || dob)}
+        maximumDate={today}
+      />
+
+      <Button
+        title="Next"
+        onPress={() =>
+          Alert.alert(
+            "Confirm Your Birthdate",
+            `You entered ${dob.toDateString()}. You can only change this once, so be sure it is accurate.`,
+            [{ text: "Go back" }, { text: "Confirm" }]
+          )
+        }
+      />
+
+      <StatusBar />
+    </View>
+  );
+};
 
 export default CreateYourAccount;
