284b7ab2562ae4547d19541e08f135fdfc7e0d09
Emma <emmanentli@gmail.com>
2023-01-16 19:04:03 -0800

    Added global styles for the welcome page

---
+++ globalStylesheet.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "black",
+        height: "100%",
+    }, 
+    titleText: {
+        textAlign: "center",
+        color: "white",
+        fontSize: 20,
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    bodyText: {
+        textAlign: "center",
+        color: "white",
+        fontSize: 14,
+    },
+    footer: {
+        marginBottom: 50,
+    },
+    bigBluebutton: {
+        margin: 10,
+        padding: 20,
+        textAlign: "center",
+        backgroundColor: "#006ee6",
+        borderRadius: 8,
+    },
+    bigBlueButtonText: {
+        textAlign: "center",
+        color: "white"
+    }
+})
+
+export default styles;
--- screens/welcome.tsx
+++ screens/welcome.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Pressable, ScrollView, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import styles from "./../globalStylesheet";
 
 type WelcomeProps = {
   navigation: any;
@@ -10,13 +11,20 @@ const Welcome: React.FC<WelcomeProps> = (props) => {
   const { navigation } = props;
 
   return (
-    <View>
-      <Text>Bent</Text>
-      <Text>Find your queer community</Text>
-      <Button
-        title="Sign up"
-        onPress={() => navigation.navigate("CreateYourAccount")}
-      />
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.titleText}>Bent</Text>
+        <Text style={styles.bodyText}>Find your queer community</Text>
+      </ScrollView>
+
+      <View style={styles.footer}>
+        <Pressable
+          style={styles.bigBluebutton}
+          onPress={() => navigation.navigate("CreateYourAccount")}
+        >
+          <Text style={styles.bigBlueButtonText}>Sign Up</Text>
+        </Pressable>
+      </View>
       <StatusBar />
     </View>
   );
